a081b5cb244574e3431bfbecd0530167069fffcf
ly <liuyao@21tb.com>
2023-10-07 17:36:27 +0800

    translate: support sub frame

---
+++ chrome-translate/inject_all.js
@@ -0,0 +1,14 @@
+document.onmouseup = function (e) {
+    sendToBackground('src', document.getSelection().toString());
+}
+
+function sendToBackground(type, data) {
+    /**
+     * chrome.runtime.id
+     * 文档： https://developer.chrome.com/docs/extensions/reference/runtime/#method-sendMessage
+     */
+    chrome.runtime.sendMessage({type: type, data: data}, {})
+        .then(e => console.log(e))
+        .catch(e => console.log(e))
+    ;
+}
